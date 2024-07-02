2204c59353b453ad9fdc2d6781b2c36b4e711ed8
ritacarrilho <ritaclameira@gmail.com>
2024-07-02 17:54:27 +0200

    fix sonar file

--- app/database.py
+++ app/database.py
@@ -7,8 +7,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://taskuser:taskpassword@localhost/taskdb')
-# DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://taskuser:taskpassword@tasks-db/taskdb')
+#DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://taskuser:taskpassword@localhost/taskdb')
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://taskuser:taskpassword@tasks-db/taskdb')
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
