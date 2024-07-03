cf0de4a69d6364a3854f3d9735717b53fc12de2c
ritacarrilho <ritaclameira@gmail.com>
2024-07-03 09:59:39 +0200

    added env file

--- tests/test_database.py
+++ tests/test_database.py
@@ -9,8 +9,7 @@ from app.database import get_db, DATABASE_URL
 
 def test_database_url():
     from app.database import DATABASE_URL
-    assert DATABASE_URL == 'postgresql://taskuser:taskpassword@tasks-db/taskdb'
-    #assert DATABASE_URL == 'postgresql://taskuser:taskpassword@localhost/taskdb'
+    assert DATABASE_URL == os.getenv('DATABASE_URL')
 
 
 def test_get_db():
